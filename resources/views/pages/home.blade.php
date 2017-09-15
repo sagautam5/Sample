bafb2f013df66d89153f9ddd5525777aae7b32aa
sagautam5 <sagautam5@gmail.com>
2017-09-15 17:37:37 +0545

    minor changes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,7 +14,7 @@
 	<div class="row">
 		<div class="col-md-12">
 			<div class="col-md-6">
-				<h1>About Me</h1>
+				<h1>About Us</h1>
 				<div class="col-md-3 logo-box">
 					<img class="logo" src="{{asset('image/sagar.jpg')}}">
 				</div>
